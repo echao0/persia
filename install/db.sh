c6459263ad9a0bdaf65d9e3a296f7a39b2242a95
echao0 <cesar@redminima.com>
2019-12-09 11:59:53 +0100

    Cambios a ser confirmados: 	modificado:     Dockerfile 	modificado:     comandos 	modificado:     db.sh 	modificado:     on.py

--- install/db.sh
+++ install/db.sh
@@ -8,9 +8,8 @@ MAINDB=${persia}
 # If /root/.my.cnf exists then it won't ask for root password
 
     mysql -e "CREATE DATABASE persia /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-    mysql -e "CREATE USER persia@localhost IDENTIFIED BY 'persia';"
-    mysql -e "GRANT ALL PRIVILEGES ON persia.* TO 'persia'@'localhost';"
+    mysql -e "CREATE USER persia@localhost IDENTIFIED BY '/Persia/';"
+    mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'persia'@'localhost';"
     mysql -e "FLUSH PRIVILEGES;"
-
+    mysql -e "persia < /opt/persia/DataBase/persia.sql"
 # If /root/.my.cnf doesn't exist then it'll ask for root password
-
